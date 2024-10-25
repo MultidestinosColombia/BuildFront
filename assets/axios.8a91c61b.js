43cdb3b9002dfbe7b8957b08d543d1c402a5a549
JulianTG10 <108954766+JulianTG10@users.noreply.github.com>
2024-10-24 22:12:48 -0500

    version03

---
+++ assets/axios.8a91c61b.js
@@ -0,0 +1,1 @@
+import{b as i}from"./index.8754f26c.js";import{a}from"./axios.37d53be1.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
